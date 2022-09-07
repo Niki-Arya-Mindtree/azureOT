184a175347f172f67578b71114a0c73e86484b6c
Niki-Arya-Mindtree <M1092216@mindtree.com>
2022-09-07 20:07:18 +0530

    Azure connected to Git With Spring boot to display

---
+++ src/main/java/com/example/demo/AzuurrnodockOtApplication.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class AzuurrnodockOtApplication {
+
+	@GetMapping
+	public String print() 
+	{return "Hello Azure and Github";}
+	
+	public static void main(String[] args) {
+		SpringApplication.run(AzuurrnodockOtApplication.class, args);
+	}
+
+}
